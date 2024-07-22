3cf5a1b7ccc41d7a7ac4e0857c44368580ea4f08
Tetiana Kobzar <tkobzar.smm@gmail.com>
2024-07-22 11:46:13 +0100

    add oll

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+from src.contacts.routers import router as router_contacts
+
+app = FastAPI()
+
+app.include_router(router_contacts, prefix="/contacts", tags=["contacts"])
